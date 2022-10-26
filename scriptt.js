ef73359573cbac8c92d7d6938cb59a9b62761bc3
elmurzaev <elmurzaev@gmail.com>
2022-10-26 16:22:28 +0300

    dwo commit

---
+++ scriptt.js
@@ -0,0 +1,19 @@
+//1)
+let now = new Date
+console.log (now)
+//2)
+//var x = "ПИ" + 1 + 2.14;
+//3)
+const float g = 9.8F;
+g =10.2;
+//4)
+let name = prompt ("expert") ;
+let address = prompt ("Tevzana");
+let phone = prompt (89899998988);
+let presentation =`Студент ${name}  адрес ${address}  номер телефона ${phone}`;
+console.log( presentation);
+//5)
+let rubles = 18;
+let dollars =60;
+let count = rubles + dollars
+console.log(count);
